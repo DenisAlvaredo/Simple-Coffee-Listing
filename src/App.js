d3e21370e46875bd4218e4486b640543c9cb0315
DenisAlvaredo <78327413+DenisAlvaredo@users.noreply.github.com>
2023-12-07 17:00:38 -0300

    all css implemented

--- src/App.js
+++ src/App.js
@@ -4,7 +4,16 @@ import CoffeeList from './components/CoffeeList';
 function App() {
   return (
     <div className="App">
-      <CoffeeList />
+      <div id='data-container'>
+        <div id='header'>
+          <h1 id='title'>Our Collection</h1>
+          <p id='quote'>
+            Introducing our Coffee Collection, a selection of unique coffees from different roast types and origins, 
+            expertly roasted in small batches and shipped fresh weekly.
+          </p>
+        </div>
+        <CoffeeList />
+      </div>
     </div>
   );
 }
--- src/components/CoffeeList.js
+++ src/components/CoffeeList.js
@@ -3,6 +3,8 @@ import './styles.css'
 
 const CoffeeList = () => {
     const [coffeeData, setCoffeeData] = useState([]);
+    const [showAll, setShowAll] = useState(true);
+
 
     useEffect(() =>{
         fetch('https://raw.githubusercontent.com/devchallenges-io/web-project-ideas/main/front-end-projects/data/simple-coffee-listing-data.json')
@@ -11,34 +13,50 @@ const CoffeeList = () => {
             .catch(error => console.error('Error fetching coffee data:', error));
     }, []);
 
+    const handleShowAll = () => {
+        setShowAll(true);
+    };
+    
+    const handleShowAvailable = () => {
+        setShowAll(false);
+    };
+
     return (
-        <div id="coffee-container">
-                {coffeeData.map(coffee => (
+        <>
+            <div id='buttons'>
+                <button onClick={handleShowAll} className={`btn ${showAll ? 'active' : ''}`}>All Products</button>
+                <button onClick={handleShowAvailable} className={`btn ${!showAll ? 'active' : ''}`}>Available Now</button>
+            </div>
+            <div id="coffee-container">
+                {coffeeData
+                .filter((coffee) => (showAll ? true : coffee.available))
+                .map(coffee => (
                     <div key={coffee.id} id="coffee-card">
-                        <img src={coffee.image} alt={coffee.name}/>
+                        <img src={coffee.image} alt={coffee.name} id="coffee-img"/>
                         {coffee.popular && <p id="popularity">Popular</p>}
                         <>
                             <div id="coffee-data">
-                                <h2>{coffee.name}</h2>
-                                <p>{coffee.price}</p>
+                                <h2 id="coffee-name">{coffee.name}</h2>
+                                <p id="coffee-price">{coffee.price}</p>
                             </div>
                         </>
                         {coffee.rating !== null ? (
-                            <div id="coffee-rarting">
+                            <div id="coffee-rating">
                                 <img src='./Star_fill.svg' alt="Rating Image" />
                                 <p id="coffee-punt">{coffee.rating}</p>
                                 <p id="coffee-votes">({coffee.votes} votes)</p>
                             </div>
                         ) : (
-                            <div id="coffee-rarting">
+                            <div id="coffee-rating">
                                 <img src='./Star.svg' alt="Default Image" />
-                                <p id="coffee-no-rating">No Rantings</p>
+                                <p id="coffee-votes">No Ratings</p>
                             </div>
                         )}
-                        {!coffee.available && <p id="coffee-unavailable">Sold Out!</p>}
+                        {!coffee.available && <p id="coffee-unavailable">Sold Out</p>}
                     </div>
                 ))}
-        </div>
+            </div>
+        </>
     );
 };
 
